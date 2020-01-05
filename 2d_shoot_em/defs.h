9bf86d592e12e876dde2eca5b81b8620d01730b3
Adam Clare <adam.jarrett.clare@gmail.com>
2020-01-05 14:57:01 -0700

    Initial setup of library and executable

---
+++ 2d_shoot_em/defs.h
@@ -0,0 +1,9 @@
+#ifndef ATC_DEFS_H
+#define ATC_DEFS_H
+
+#include "SDL2/SDL.h"
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+#endif
--- 2d_shoot_em/main.c
+++ 2d_shoot_em/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "SDL2/SDL.h"
+#include "defs.h"
 
 int main() {
   printf("Hello from game_tutorials\n");
