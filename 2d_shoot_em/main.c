d6f9b9c682cf07adccebd35a338a9cbc59085518
Adam Clare <adam.jarrett.clare@gmail.com>
2020-01-05 14:51:18 -0700

    Simple test to make sure sdl is linked correctly

--- 2d_shoot_em/main.c
+++ 2d_shoot_em/main.c
@@ -3,4 +3,10 @@
 
 int main() {
   printf("Hello from game_tutorials\n");
+  /* Test to make sure SDL is being linked correctly */
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("Couldn't initialize SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+  printf("SDL linked correctly\n");
 }
